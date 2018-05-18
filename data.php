dd9263e0b1c3379ec0122fe5dab9577fdc25e088
Alessandro Leita <alessandro.leita89@gmail.com>
2018-05-18 17:42:11 +0200

    Milestone 2 primo grafico finito

--- data.php
+++ data.php
@@ -10,26 +10,54 @@
 
     <?php
 
-    $data = [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322];
+
+
+
+    $graphs = [
+      'fatturato' => [
+        'type' => 'line',
+        'data' => [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322]
+      ],
+      'fatturato_by_agent' => [
+        'type' => 'pie',
+        'data' => [
+          'Marco' => 9000,
+          'Giuseppe' => 4000,
+          'Mattia' => 3200,
+          'Alberto' => 2300
+        ]
+      ]
+    ];
+
+  /* $data = [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322];*/
 
 
      ?>
+     <?php
+     echo json_encode($graphs['fatturato']);
+      ?>
+
+
 
      <canvas id="myChart"></canvas>
+     <canvas id="myChart2"></canvas>
+
 
      <script type="text/javascript">
      <?php include 'php-charts/data.php' ?>
      $(document).ready(function() {
+       var primoGrafico = <?php echo json_encode($graphs['fatturato']); ?>;
+       var secondoGrafico = <?php echo json_encode($graphs['fatturato_by_agent']); ?>;
        var ctx = document.getElementById('myChart').getContext('2d');
        var chart = new Chart(ctx,{
-         type :'line',
+         type : primoGrafico.type,
          data : {
            labels: ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
            datasets: [{
-             label: 'Vendite mensili',
+             label: 'andamento mensile vendite',
              backgroundColor: 'rgb(255, 99, 132)',
              borderColor: 'rgb(255, 99, 132)',
-             data: <?php echo json_encode($data); ?>,
+             data: primoGrafico.data,
            }],
          },
 
@@ -39,8 +67,6 @@
      })
 
      </script>
-
-
 
   </body>
 </html>
